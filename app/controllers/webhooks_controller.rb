e921cbd3306072276874bfa26f62f7e60539ef4f
James <blue.rockcrm@gmail.com>
2018-01-22 12:46:50 +0500

    Webhooks controller for helpscout

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,6 @@
+class WebhooksController < ApplicationController
+
+  def helpscout
+    render layout: false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  resources :webhooks, only: :none do
+    collection do
+      get :helpscout
+    end
+  end
 end
